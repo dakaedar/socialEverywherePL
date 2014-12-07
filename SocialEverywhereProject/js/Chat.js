a47e9597d19a0b4340efb5c7d6e4d6f98406475c
unknown <aubole@gmail.com>
2014-12-07 10:08:49 +0900

    gestion private room fini

--- SocialEverywhereProject/js/Chat.js
+++ SocialEverywhereProject/js/Chat.js
@@ -1,82 +1,121 @@
 
 	/* CHAT ROOM */
-	document.getElementById("addContact").onclick=function addContact()
+	function addContactRoom(id)
 	{
-		$("<img src='img/photoProfil.jpg' alt='Photo contact' width='20' height='20'><span> Contact name<br></span><button>+</button><br>").insertBefore('.contact div');
+		roomId = "#contactRoom" + id;
+		$("<img src='img/photoProfil.jpg' alt='Photo contact' width='20' height='20'><span>Contact name<br></span><button>+</button><br>").insertBefore(roomId + ' div');
+	}
+	
+	document.getElementById("addContact1").onclick=function addContact1()
+	{
+		addContactRoom(1);
+	}
+	
+	document.getElementById("addContact2").onclick=function addContact2()
+	{
+		addContactRoom(2);
+	}
+
+
+	document.getElementById("addContact3").onclick=function addContact3()
+	{
+		addContactRoom(3);
+	}
+
+
+	document.getElementById("addContact4").onclick=function addContact4()
+	{
+		addContactRoom(4);
+	}
+
+
+	document.getElementById("addContact5").onclick=function addContact5()
+	{
+		addContactRoom(5);
 	}
 
 	document.getElementById("displayRoom1").onclick=function displayRoom1()
 	{
 		$("#chatRoom1").css('display', 'block');
-//		$("#contentChat #contactRoom1").css('display', 'block');
 		$("#chatRoom2").css('display', 'none');
-//		$("#contentChat #contactRoom2").css('display', 'none');
 		$("#chatRoom3").css('display', 'none');
-//		$("#contentChat #contactRoom3").css('display', 'none');
 		$("#chatRoom4").css('display', 'none');
-//		$("#contentChat #contactRoom4").css('display', 'none');
 		$("#chatRoom5").css('display', 'none');
-//		$("#contentChat #contactRoom5").css('display', 'none');
+
+		$("#contentChat #contactRoom1").css('display', 'block');
+		$("#contentChat #contactRoom2").css('display', 'none');
+		$("#contentChat #contactRoom3").css('display', 'none');
+		$("#contentChat #contactRoom4").css('display', 'none');
+		$("#contentChat #contactRoom5").css('display', 'none');
+
 		$(".contact div").css('display', 'none');
 	}
 	
 	document.getElementById("displayRoom2").onclick=function displayRoom2()
 	{
 		$("#chatRoom1").css('display', 'none');
-//		$("#contentChat #contactRoom1").css('display', 'none');
 		$("#chatRoom2").css('display', 'block');
-//		$("#contentChat #contactRoom2").css('display', 'block');
 		$("#chatRoom3").css('display', 'none');
-//		$("#contentChat #contactRoom3").css('display', 'none');
 		$("#chatRoom4").css('display', 'none');
-//		$("#contentChat #contactRoom4").css('display', 'none');
 		$("#chatRoom5").css('display', 'none');
-//		$("#contentChat #contactRoom5").css('display', 'none');
+
+		$("#contentChat #contactRoom1").css('display', 'none');
+		$("#contentChat #contactRoom2").css('display', 'block');
+		$("#contentChat #contactRoom3").css('display', 'none');
+		$("#contentChat #contactRoom4").css('display', 'none');
+		$("#contentChat #contactRoom5").css('display', 'none');
+
 		$(".contact div").css('display', 'block');
 	}
 	
 	document.getElementById("displayRoom3").onclick=function displayRoom3()
 	{
 		$("#chatRoom1").css('display', 'none');
-//		$("#contentChat #contactRoom1").css('display', 'none');
 		$("#chatRoom2").css('display', 'none');
-//		$("#contentChat #contactRoom2").css('display', 'none');
 		$("#chatRoom3").css('display', 'block');
-//		$("#contentChat #contactRoom3").css('display', 'block');
 		$("#chatRoom4").css('display', 'none');
-//		$("#contentChat #contactRoom4").css('display', 'none');
 		$("#chatRoom5").css('display', 'none');
-//		$("#contentChat #contactRoom5").css('display', 'none');
+
+		$("#contentChat #contactRoom1").css('display', 'none');
+		$("#contentChat #contactRoom2").css('display', 'none');
+		$("#contentChat #contactRoom3").css('display', 'block');
+		$("#contentChat #contactRoom4").css('display', 'none');
+		$("#contentChat #contactRoom5").css('display', 'none');
+
 		$(".contact div").css('display', 'block');
 	}
 
 	document.getElementById("displayRoom4").onclick=function displayRoom4()
 	{
 		$("#chatRoom1").css('display', 'none');
-//		$("#contentChat #contactRoom1").css('display', 'none');
 		$("#chatRoom2").css('display', 'none');
-//		$("#contentChat #contactRoom2").css('display', 'none');
 		$("#chatRoom3").css('display', 'none');
-//		$("#contentChat #contactRoom3").css('display', 'none');
 		$("#chatRoom4").css('display', 'block');
-//		$("#contentChat #contactRoom4").css('display', 'block');
 		$("#chatRoom5").css('display', 'none');
-//		$("#contentChat #contactRoom5").css('display', 'none');
+
+		$("#contentChat #contactRoom1").css('display', 'none');
+		$("#contentChat #contactRoom2").css('display', 'none');
+		$("#contentChat #contactRoom3").css('display', 'none');
+		$("#contentChat #contactRoom4").css('display', 'block');
+		$("#contentChat #contactRoom5").css('display', 'none');
+
 		$(".contact div").css('display', 'block');
 	}
 
 	document.getElementById("displayRoom5").onclick=function displayRoom5()
 	{
 		$("#chatRoom1").css('display', 'none');
-//		$("#contentChat #contactRoom1").css('display', 'none');
 		$("#chatRoom2").css('display', 'none');
-//		$("#contentChat #contactRoom2").css('display', 'none');
 		$("#chatRoom3").css('display', 'none');
-//		$("#contentChat #contactRoom3").css('display', 'none');
 		$("#chatRoom4").css('display', 'none');
-//		$("#contentChat #contactRoom4").css('display', 'none');
 		$("#chatRoom5").css('display', 'block');
-//		$("#contentChat #contactRoom5").css('display', 'block');
+
+		$("#contentChat #contactRoom1").css('display', 'none');
+		$("#contentChat #contactRoom2").css('display', 'none');
+		$("#contentChat #contactRoom3").css('display', 'none');
+		$("#contentChat #contactRoom4").css('display', 'none');
+		$("#contentChat #contactRoom5").css('display', 'block');
+
 		$(".contact div").css('display', 'block');
 	}
 
